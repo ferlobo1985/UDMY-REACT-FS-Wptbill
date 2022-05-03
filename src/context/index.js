e87c366cb810cba34b32aede242bff9de7ff534f
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-03 15:34:37 -0300

    04-Show list and remove

--- src/context/index.js
+++ src/context/index.js
@@ -19,12 +19,19 @@ class MyProvider extends Component {
         }))
     }
 
+    removePlayerHandler = (idx) => {
+        let newArray = this.state.players;
+        newArray.splice(idx,1);
+        this.setState({players:newArray});
+    }
+
     render(){
         return(
             <>
                 <MyContext.Provider value={{
                     state: this.state,
-                    addPlayer: this.addPlayerHandler
+                    addPlayer: this.addPlayerHandler,
+                    removePlayer: this.removePlayerHandler
                 }}>
                     {this.props.children}
                 </MyContext.Provider>
